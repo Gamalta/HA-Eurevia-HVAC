5f05132f1d9eff80104ba3d13e3088e39da0422f
Gamalta <eliovital@icloud.com>
2025-06-08 14:41:49 +0200

    fix: coordinator

--- climate.py
+++ climate.py
@@ -14,7 +14,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
         if all(k in data for k in ("ID", "Mode_Active", "Stp_Comf", "Zone_Name")):
             async_add_entities([EureviaClimate(coordinator)])
 
-    for coordinator in hass.data[DOMAIN].values():
+    for coordinator in hass.data[DOMAIN]["coordinators"].values():
         await add_coordinator(coordinator)
 
     async_dispatcher_connect(hass, f"{DOMAIN}_new_device", add_coordinator)
